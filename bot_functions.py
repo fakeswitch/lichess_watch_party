99d0e0e0e9d56516dd5bcb640157afa13355eeae
fakeswitch <anythingisaac@gmail.com>
2022-04-03 00:00:38 -0700

    catching public repo up to production repo

--- bot_functions.py
+++ bot_functions.py
@@ -2,6 +2,7 @@
 from sqlite3 import dbapi2
 import mysql.connector
 import json
+import ndjson
 
 #----------- dealign with web settings ---------------
 # Ignore SSL certificate errors
@@ -30,8 +31,8 @@ def get_in_play():
     rstr =""
     for i in mycursor: players.append(i[0])
     if len(players) > 0:
-        for x in players: rstr = rstr + ", " +  x
-        rstr = "Plaing now on Lichess: " + rstr[2:]  
+        for x in players: rstr = rstr + ", " +  "https://lichess.org/@/{}/tv".format(x)
+        rstr = "Playing now on Lichess: " + rstr[2:]  
     else:
         rstr = "There is currently no one from the Watch Party List playing on Lichess"
     return rstr
@@ -84,15 +85,27 @@ def follow_player(content, discord_id):
     else:
         retr_str = "Please let me know who you would like to follow"
     return retr_str
-    
 
-# adds discord user to list of lichess players to follow
+# for people who want to follow eveyrone on the list... 
+def follow_everyone(discord_id):
+    mydb = get_db()
+    mycursor = mydb.cursor()
+    isrt_str = """
+    insert into followers(lichess_name, discord_id)
+    select handle, '{}' from watch_users where handle not in
+    (select lichess_name from followers where discord_id = '{}');
+    """
+    mycursor.execute(isrt_str.format(discord_id, discord_id))
+    mydb.commit()
+    return "*sigh*... okay you are now following everyone on the current roster"
+    
+# remvoes the discord user/lichess_name pair from followers table 
 def unfollow_player(content, discord_id):
     inp = content.split()
     if len(inp) > 1:
         lichess = inp[1]
         if check_player("holder " + lichess).startswith("Y"):
-            retr_str ="Okay, More notifications for when " +lichess.strip() + " starts playing on Lichess"
+            retr_str ="Okay, No more notifications for when " +lichess.strip() + " starts playing on Lichess"
             mydb = get_db()
             mycursor = mydb.cursor()
             sql_str = "delete from followers where lichess_name = '" +lichess.strip() + "' and discord_id = '" + discord_id.strip() +"';" 
@@ -102,6 +115,16 @@ def unfollow_player(content, discord_id):
             retr_str = lichess + " is not on the watch party list"
     else:
         retr_str = "Please let me know who you'd like to unfollow"
+    return retr_str
+
+# removes every record from the followers table with the specified discord id
+def unfollow_everyone(discord_id):
+    mydb = get_db()
+    mycursor = mydb.cursor()    
+    del_str ="delete from followers where discord_id = '" + discord_id + "';"
+    mycursor.execute(del_str)
+    mydb.commit()
+    retr_str = "You are no longer following anyone"
     return retr_str
 
 #returns a list of lichess players discord user is currently following
@@ -120,6 +143,7 @@ def get_my_follows(discord_id):
         retr_str = "You are not following anyone"
     return retr_str
 
+# adds lichess handle to watch_users table
 def add_player(lichess):
     retr_str = "something went wrong"
     mydb = get_db()
@@ -133,19 +157,23 @@ def add_player(lichess):
     if x > 0:
         retr_str = lichess + " is already on the watch list"
     else:
+        #check to see if id exists on lichess
         url = 'https://lichess.org/api/users/status?ids=' + lichess.strip()
         html = urlopen(url, context=ctx).read()
         stat = json.loads(html.decode('utf-8'))
         if len(stat)>0:
+            # if lichess finds id, add to the watch_users table
             mycursor = mydb.cursor()
             ist_str = "insert into watch_users(handle, status) values ('" + lichess.strip() + "', False);"
             mycursor.execute(ist_str)
             mydb.commit()
             retr_str = lichess + " is now part of the watch list."
         else:
+            #lichess did not find specified handle
             retr_str = lichess + " does not appear to be a Lichess Handle"
     return retr_str
 
+# remove handle from watch_users table
 def remove_player(lichess):
     retr_str = ""
     mydb = get_db()
@@ -154,24 +182,98 @@ def remove_player(lichess):
     sql_str = "select count(*) from watch_users where handle = '" + lichess.strip() + "';"
     x = None
     mycursor.execute(sql_str)
+    # check to see if handle is in table
     for i in mycursor: 
         x = i[0]
     if x > 0:
+        # removes handle if exits
         mycursor = mydb.cursor()
         del_str = "delete from watch_users where handle = '" + lichess.strip() + "';"
         mycursor.execute(del_str)
         mydb.commit()
         retr_str = lichess + " has been removed the watch party list"
     else:
+        # handle not found
         retr_str = lichess + " is not currently part of the watch party list"
     return retr_str
 
+#--------- get user stat functions ---------
+
+# get player top 3 openings as white
+# expecting an list of at least one item (lichess user name), 
+# optional second item for specific time controlls
+def get_top_open(args_list):
+    lichess = args_list[0]
+    tc = "playing in all Time Controlls"
+    if len(args_list) < 2:
+        url = ' https://explorer.lichess.ovh/player?player={}&color=white'.format(lichess)
+    else:
+        url = ' https://explorer.lichess.ovh/player?player={}&color=white&speeds={}'.format(lichess,args_list[1])
+        pretty_tc = args_list[1].replace(",", ", ")
+        tc = "playing in {}".format(pretty_tc)
+    html = urlopen(url, context=ctx).read()
+    stat = ndjson.loads(html.decode('utf-8'))
+    rg = 3
+    if len(stat[0].get('moves')) < 3: 
+        rg = len(stat[0].get('moves'))
+    top3 ="Top {} openings (by times played) for {} {}:".format(rg, lichess, tc)
+    for i in range(rg):
+        move = stat[0].get('moves')[i].get('san')
+        perform = stat[0].get('moves')[i].get('performance')
+        white = stat[0].get('moves')[i].get('white')
+        black =stat[0].get('moves')[i].get('black')
+        draws = stat[0].get('moves')[i].get('draws')
+        top3 += "\n1. {} with performace of {}, and record of +{} -{} ={}".format(move, perform, white, black, draws)
+    return top3.strip()
+
+# get current ratings
+def get_cur_ratings(lichess):
+    url = 'https://lichess.org/api/user/{}'.format(lichess)
+    try:
+        html = urlopen(url, context=ctx).read()
+        user = json.loads(html.decode('utf-8'))
+        tc = ['bullet', 'blitz', 'rapid', 'classical', 'puzzle']
+        ret_string = "Current ratings for {}:".format(user.get('username'))
+        for i in tc:
+            j = user.get('perfs').get(i)
+            rating = j.get('rating')
+            rd = j.get('rd')
+            games = j.get('games')
+            ret_string += "\n{}: {} with a RD of {} in {} games".format(i.capitalize(), rating, rd, games)
+    except: ret_string = 'Could not find user {}'.format(lichess)
+    return ret_string.strip()
 
 
-# check_player("!lwpCheck fake_Switch") 
-
-# follow_player("!lwpFollow fauxtog", "fake_switch")
-# follow_player("!lwpFollow DrNykterstein", "fake_switch")
-# get_my_follows("fauxtog")
-# get_my_follows("fake_switch")
-# unfollow_player("!lwpunFollow DrNyktersteiasdfasdfn", "fake_switch")
+#---------- silly non chess related functions---------
+def make_haiku():
+    mydb = get_db()
+    mycursor = mydb.cursor()
+    mycursor.execute("use haiku;")
+    #get random 5 sylb
+    mycursor.execute("select * from rand_five limit 2;")
+    hf = ""
+    for l in mycursor: hf = hf + "," + str(l[0])
+    hf = hf[1:]
+    mycursor.execute("delete from rand_five where id in ({});".format(hf))
+    mydb.commit()
+    #get random 7 sylb
+    mycursor.execute("select * from rand_seven limit 1;")
+    hs = ""
+    for m in mycursor: hs = str(m[0])
+    mycursor.execute("delete from rand_seven where id in ({});".format(hs))
+    mydb.commit()
+    #get actual lines
+    s = []
+    l = []
+    sql1 = "select id, verse from component where id in ({});".format(hf)
+    sql2 = "select id, verse from component where id in ({});".format(hs)
+    mycursor.execute(sql1)
+    for i in mycursor: s.append((i[0],i[1]))
+    mycursor.execute(sql2)
+    for j in mycursor: l.append((j[0],j[1]))
+    #store record of the created haiku
+    sql3 = "insert into new_verses(l0_id,l1_id,l2_id) values ({},{},{})".format(s[0][0], l[0][0], s[1][0])
+    mycursor.execute(sql3)
+    mydb.commit()
+    haiku = s[0][1].lower().capitalize() +"\n" +l[0][1].lower().capitalize() + "\n" + s[1][1].lower().capitalize()
+    return haiku
--- lwp_manager.py
+++ lwp_manager.py
@@ -4,6 +4,15 @@ from discord.ext import commands
 import bot_functions as bf
 import helper as h
 
+#---------- import logging?-------
+import logging
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+#logging.basicConfig(level=logging.INFO)
 #intents = discord.Intents.default()
 #intents.members = True
 
@@ -17,6 +26,14 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+
+#------------------------------general user functrions ----------- 
+
+# get a list of commands this bot offers
+@bot.command()
+async def commands(ctx):
+    retr_str = "^playing, ^list, ^mylist, ^check lichess_user_name, ^follow lichess_user_name, ^follow_all, ^unfollow lichess_user_name, ^unfollow_all, ^add lichess_user_name, ^remove lichess_user_name, ^openings lichess_user_name"
+    await ctx.send(retr_str)
 
 # who is currently playing
 @bot.command()
@@ -38,15 +55,32 @@ async def mylist(ctx):
 async def check(ctx, lichess):
     await ctx.send(bf.check_player(ctx.message.content))
 
+
+#-------------------follow/unfollow functions for discord ----------- 
+
 # follow a licess user
 @bot.command()
 async def follow(ctx, lichess):
     await ctx.send(bf.follow_player(ctx.message.content, str(ctx.author.mention)))
 
+# i've made a mistake and i want to follow anyone
+@bot.command()
+async def follow_all(ctx):
+    retr_str = bf.follow_everyone(str(ctx.author.mention))
+    await ctx.send(retr_str)
+
 # list user is currently following
 @bot.command()
 async def unfollow(ctx, lichess):
     await ctx.send(bf.unfollow_player(ctx.message.content,str(ctx.author.mention)))
+
+# i've made a mistake and no longer want to follow anyone
+@bot.command()
+async def unfollow_all(ctx):
+    retr_str = bf.unfollow_everyone(str(ctx.author.mention))
+    await ctx.send(retr_str)
+
+#-------------------add/remove from watch list functions for mods ----------- 
 
 # add lichess user to watch party master list
 # restricted to people who have permission to ban members 
@@ -66,8 +100,21 @@ async def remove(ctx, lichess):
         retr_str = bf.remove_player(lichess)
     await ctx.send(retr_str)
 
+#---------------------- player stat commands   
+# get top 3 opening moves of a player when they play white
 @bot.command()
-async def commands(ctx):
-    retr_str = "^playing, ^list, ^mylist, ^check, ^follow lichess_user_name, ^unfollow lichess_user_name, ^add lichess_user_name, ^remove lichess_user_name"
-    await ctx.send(retr_str)
+async def openings(ctx, *args):
+    await ctx.send(bf.get_top_open(args))
+
+# get ratings of bullet/blitz/rapid/classical/puzzles of a player
+@bot.command()
+async def ratings(ctx, user):
+    await ctx.send(bf.get_cur_ratings(user))
+
+#---------------------- silly commands
+@bot.command()
+async def haiku(ctx):
+    await ctx.send(bf.make_haiku())
+
+#------------------------- connect bot to discord-------------
 bot.run(h.bat)
